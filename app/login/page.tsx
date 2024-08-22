3e7c16aa3ae7224b7a83ab4dd4d8b9b90b0f4bb8
itagmi <itagmi88@gmail.com>
2024-08-22 18:02:32 +0900

    :sparkcles: server action - form post로 요청 받은거 server에서 받기

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,12 @@
-"use client";
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
-  const onClick = async () => {
-    const response = await fetch("/www/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "nico",
-        paasword: "1234",
-      }),
-    });
-    console.log(await response.json());
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+    console.log("hey im running in server baby!");
   };
   return (
     <section className="flex flex-col gap-10 py-8 px-6">
@@ -20,13 +14,23 @@ export default function Login() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in width email and password</h2>
       </div>
-      <form action="" className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required error={[]} />
-        <FormInput type="password" placeholder="password" required error={[]} />
-      </form>
-      <span onClick={onClick}>
+      <form action={handleForm} className="flex flex-col gap-3">
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          error={[]}
+        />
+        <FormInput
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          error={[]}
+        />
         <FormButton loading={false} text="Create account" />
-      </span>
+      </form>
       <SocialLogin />
     </section>
   );
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,16 +3,19 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   error: string[];
+  name: string;
 }
 export default function FormInput({
   type,
   placeholder,
   required,
   error,
+  name,
 }: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
+        name={name}
         type={type}
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
         placeholder={placeholder}
